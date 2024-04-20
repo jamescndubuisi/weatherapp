a8245d1508aead21c884b61ca404dcfd97c6afb9
James Ndubuisi <jamescndubuisi@gmail.com>
2024-04-20 23:35:19 +0100

    first commit

---
+++ weatherapp/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+import requests
+import datetime
+
+
+def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+    print(response)
+    lat, lon = response['coord']['lat'], response['coord']['lon']
+    count = 5
+    units = "metric"
+    forecast_response = requests.get(forecast_url.format(lat, lon,  api_key, count, units)).json()
+    print(forecast_response)
+
+    weather_data = {
+        'city': city,
+        'temperature': round(response['main']['temp'] - 273.15, 2),
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+    }
+
+    daily_forecasts = []
+    for daily_data in forecast_response['list'][:5]:
+        print(daily_data)
+        data = daily_data["main"]
+        print(data)
+        daily_forecasts.append({
+            'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
+            'hour': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%I'),
+            'minutes': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%M'),
+            'ampm': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%p'),
+            'min_temp': round(data['temp_min'], 2),
+            'max_temp': round(data['temp_max'], 2),
+            'description': daily_data['weather'][0]['description'],
+            'icon': daily_data['weather'][0]['icon'],
+        })
+    print(daily_forecasts)
+    return weather_data, daily_forecasts
+
+
+def index(request):
+    # api_key = '1ebfba407737f1180cd073f9fad8abf0'
+    api_key = "7c6aa4f298cb2c7cf6bf194393331ff2"
+    current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+    forecast_url = 'https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}&cnt={}&units={}'
+    # forecast_url = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat={}&lon={}&cnt={}&appid={}'
+
+    if request.method == 'POST':
+        city1 = request.POST['city1']
+        city2 = request.POST.get('city2', None)
+
+        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, api_key, current_weather_url, forecast_url)
+
+        if city2:
+            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, api_key, current_weather_url,
+                                                                         forecast_url)
+        else:
+            weather_data2, daily_forecasts2 = None, None
+
+        context = {
+            'weather_data1': weather_data1,
+            'daily_forecasts1': daily_forecasts1,
+            'weather_data2': weather_data2,
+            'daily_forecasts2': daily_forecasts2,
+        }
+
+        return render(request, 'weather_app/index.html', context)
+    else:
+        return render(request, 'weather_app/index.html')
+
